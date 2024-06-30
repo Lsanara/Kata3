91e86ced862b8874cd3f265d8083efdab58fdbb7
Lsanara <nebeproyects@gmail.com>
2024-06-30 10:24:04 +0100

    First commit

---
+++ src/Kata3/OrganizationLoader.java
@@ -0,0 +1,7 @@
+package Kata3;
+import java.util.List;
+
+public interface OrganizationLoader {
+    List<Organization> load();
+}
+
---
+++ src/Kata3/OrganizationProcessor.java
@@ -0,0 +1,7 @@
+package Kata3;
+
+import java.util.List;
+import java.util.Map;
+public interface OrganizationProcessor {
+    Map<String,Integer> process(List<Organization> organizations);
+}
